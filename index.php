0832c30450e85cd74bafb9484b57e559b8b689ce
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-10 13:23:02 -0500

    Home page route added

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//THis is our controller
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Creat an instance of the base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
